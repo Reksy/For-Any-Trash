d1feecae4cddc7a4d3823239a038984164d47330
Anna Maslennikova <anna.maslennikova@kaspersky.com>
2022-01-15 13:34:10 +0300

    End of section 2

---
+++ Reactivities/Persistence/Migrations/20220115094130_FixColumnName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class FixColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cathegory",
+                table: "Activities",
+                newName: "Category");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Activities",
+                newName: "Cathegory");
+        }
+    }
+}
