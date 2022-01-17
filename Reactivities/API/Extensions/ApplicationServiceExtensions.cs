a44f1534275d921e492ae97282e13a2bfd7304ca
Anna Maslennikova <anna.maslennikova@kaspersky.com>
2022-01-17 18:45:25 +0300

    End  of section 5

---
+++ Reactivities/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+
+            services.AddDbContext<DataContext>(opt=>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+
+            //Подключаем CORS политику, для доступа к API с определенного URL
+            services.AddCors(opt=>{
+                opt.AddPolicy("CorsPolicy",policy=>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+            
+            //Подключаем медиатр(посредник) для доступа к операциям с базой 
+            services.AddMediatR(typeof(List.Handler).Assembly);
+
+            //Подключаем AutoMapper для маппинга полей при запросах
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
