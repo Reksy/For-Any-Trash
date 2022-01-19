3ac8d9da7815a4aa2640a7d092f53e14aa4f24af
Anna Maslennikova <anna.maslennikova@kaspersky.com>
2022-01-19 21:17:03 +0300

    End of section 7

--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -24,7 +24,7 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
     post: <T> (url: string, body = {}) => axios.post <T>(url, body).then(responseBody),
-    put: <T> (url: string, body = {}) => axios.put(url, body).then(responseBody),
+    put: <T> (url: string, body = {}) => axios.put<T>(url, body).then(responseBody),
     del: (url: string) => axios.delete(url).then(responseBody),
 }
 
--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,16 +1,13 @@
-import React, { ChangeEvent, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { ChangeEvent, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity)=>void;
-    submitting: boolean;
-}
 
-export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit,submitting }: Props) {
 
+export default observer(function ActivityForm() {
+    const { activityStore } = useStore();
+    const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
     const initialState = selectedActivity ?? {
         id: '',
         category: '',
@@ -24,9 +21,7 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
     const [activity, setActivity] = useState(initialState);
 
     function handleSubmit() {
-        console.log(activity);
-        createOrEdit(activity);
-
+        activity.id ? updateActivity(activity) : createActivity(activity);
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
@@ -39,14 +34,14 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete='off'>
                 <Form.Input placeholder='Title' value={activity.title} onChange={handleInputChange} name='title' />
-                <Form.TextArea placeholder='Description' value={activity.description} onChange={handleInputChange} name='description'/>
-                <Form.Input placeholder='Category' value={activity.category} onChange={handleInputChange} name='category'/>
-                <Form.Input type='date' placeholder='Date' value={activity.date} onChange={handleInputChange} name='date'/>
-                <Form.Input placeholder='City' value={activity.city} onChange={handleInputChange} name='city'/>
-                <Form.Input placeholder='Venue' value={activity.venue} onChange={handleInputChange} name='venue'/>
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                <Form.TextArea placeholder='Description' value={activity.description} onChange={handleInputChange} name='description' />
+                <Form.Input placeholder='Category' value={activity.category} onChange={handleInputChange} name='category' />
+                <Form.Input type='date' placeholder='Date' value={activity.date} onChange={handleInputChange} name='date' />
+                <Form.Input placeholder='City' value={activity.city} onChange={handleInputChange} name='city' />
+                <Form.Input placeholder='Venue' value={activity.venue} onChange={handleInputChange} name='venue' />
+                <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
-}
+});
