1855e1da89c113ba084da42477d3d2a8083a4647
Anna Maslennikova <anna.maslennikova@kaspersky.com>
2022-02-28 13:21:36 +0300

    Промежуточный коммит

--- Reactivities/Application/Activities/Create.cs
+++ Reactivities/Application/Activities/Create.cs
@@ -1,4 +1,6 @@
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -6,12 +8,12 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command: IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -19,14 +21,24 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public class CommandValidator : AbstractValidator<Command>
             {
-                 _context.Activities.Add(request.Activity);
-                 
-                 await _context.SaveChangesAsync();
+                public CommandValidator()
+                {
+                    RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
 
+                }
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.Activity);
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if(!result) return Result<Unit>.Failure("Failed to create an activity");
                 // Equal to nothing
-                 return Unit.Value;
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -1,39 +1,55 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import { history } from "../..";
 import { Activity } from "../models/activity";
 
-const sleep=(delay:number)=>{
-    return new Promise((resolve)=>{
-        setTimeout(resolve,delay);
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
     })
 }
 
 
 axios.defaults.baseURL = 'https://localhost:5001/api';
 
-axios.interceptors.response.use(async response=>{
-    return sleep(1000).then(()=>{
-        return response;
-    }).catch((error)=>{
-        console.log(error);
-        return Promise.reject(error);
-    })
+axios.interceptors.response.use(async response => {
+    await sleep(1000);
+    return response;
+}, (error: AxiosError) => {
+    const { data, status } = error.response!;
+    switch (status) {
+        case 400:
+            toast.error("Bad request");
+            break;
+        case 401:
+            toast.error("unauthorised")
+            break;
+        case 404:
+            history.push("/not-found");
+            break;
+        case 500:
+            toast.error("server error")
+            break;
+    }
+
+    return Promise.reject(error);
 });
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body = {}) => axios.post <T>(url, body).then(responseBody),
-    put: <T> (url: string, body = {}) => axios.put<T>(url, body).then(responseBody),
+    post: <T>(url: string, body = {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body = {}) => axios.put<T>(url, body).then(responseBody),
     del: (url: string) => axios.delete(url).then(responseBody),
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
-    details: (id:string)=> requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity)=> axios.post<void>('/activities', activity),
-    update: (activity: Activity)=> axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string)=> axios.delete<void>(`/activities/${id}`)
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => axios.post<void>('/activities', activity),
+    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
 const agent = {
