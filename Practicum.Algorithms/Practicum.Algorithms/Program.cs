d7a0685deadebac0865d300984b26c9cd8924569
Anna Maslennikova <anna.maslennikova@kaspersky.com>
2022-06-06 11:16:32 +0300

    1

---
+++ Practicum.Algorithms/Practicum.Algorithms/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Solution
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var n = ReadInt();
+        var arrA = ReadList();
+        var k = ReadInt();
+
+        writer.WriteLine(String.Join(" ", MovingAvg(arrA, k)));
+        reader.Close();
+        writer.Close();
+    }
+
+    public static List<double> MovingAvg(List<int> arr, int k)
+    {
+        var result = new List<double>();
+
+        double sum = 0;
+        for (var i = 0; i < k; i++)
+        {
+            sum += arr[i];
+        }
+        result.Add(sum / k);
+
+        for (var i=0; i< arr.Count - k; i++)
+        {
+            sum -= arr[i];
+            sum+=arr[i+k];
+            result.Add(sum / k);
+        }
+
+        return result;
+    }
+
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+}
---
+++ Practicum.Algorithms/Practicum.Algorithms/Zip.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Practicum.Algorithms
+{
+    internal class Zip
+    {
+        private static TextReader reader;
+        private static TextWriter writer;
+        public static void Run()
+        {
+            reader = new StreamReader(Console.OpenStandardInput());
+            writer = new StreamWriter(Console.OpenStandardOutput());
+
+            var n = ReadInt();
+            var arrA = ReadList();
+            var arrB = ReadList();
+
+            writer.WriteLine(String.Join(" ", Zipper(arrA, arrB)));
+            reader.Close();
+            writer.Close();
+        }
+        private static List<int> Zipper(List<int> arrA, List<int> arrB)
+        {
+            var result = new List<int>();
+
+            for (var i = 0; i < arrA.Count; i++)
+            {
+                result.Add(arrA[i]);
+                result.Add(arrB[i]);
+            }
+
+            return result;
+        }
+
+        private static int ReadInt()
+        {
+            return int.Parse(reader.ReadLine());
+        }
+
+        private static List<int> ReadList()
+        {
+            return reader.ReadLine()
+                .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+        }
+    }
+}
